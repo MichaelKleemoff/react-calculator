830bc89862328675a11474f942c6048743811384
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-06-23 15:35:20 -0400

    First commit.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+
+function App() {
+	return (
+		<div className='app'>
+			<h1>React Calculator</h1>
+		</div>
+	);
+}
+
+export default App;
